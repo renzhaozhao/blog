2e81100e3ff20155857ea495b1c7ac1a9943a36e
renzhao <3251qq@163.com>
2015-11-25 17:18:50 +0800

    bug 修改

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,25 @@ var crypto = require('crypto'),
 
 module.exports = function(app) {
 
-    
+
     app.get('/', function (req, res) {
         Post.get(null, function (err, posts){
             if (err) {
                 posts = [];
             }
-            res.render('index', { 
+            res.render('index', {
                 title: '主页',
                 user: req.session.user,
                 posts: posts,
                 success: req.flash('success').toString(),
                 error: req.flash('error').toString()
             });
-            console.log(posts[0].name);
         });
     });
 
     app.get('/reg', checkNotLogin);
     app.get('/reg', function (req, res) {
-        res.render('reg', { 
+        res.render('reg', {
             title: '注册',
             user: req.session.user,
             success: req.flash('success').toString(),
@@ -38,7 +37,7 @@ module.exports = function(app) {
         repassword = req.body['repassword'];
         //检验用户两次输入的密码是否一致
         if (repassword != password) {
-            req.flash('error', '两次输入的密码不一致!'); 
+            req.flash('error', '两次输入的密码不一致!');
             return res.redirect('/reg');//返回注册页
         }
         //生成密码的 md5 值
@@ -49,7 +48,7 @@ module.exports = function(app) {
             password: password,
             email: req.body.email
         });
-        //检查用户名是否已经存在 
+        //检查用户名是否已经存在
         User.get(newUser.name, function (err, user) {
             if (err) {
                 req.flash('error', err);
@@ -71,7 +70,7 @@ module.exports = function(app) {
             });
         });
     });
-    
+
     app.get('/login', checkNotLogin);
     app.get('/login', function (req, res) {
         res.render('login', {
@@ -91,15 +90,15 @@ module.exports = function(app) {
         //检查用户是否存在
         User.get(req.body.name, function (err, user) {
             if (!user) {
-                req.flash('error', '用户不存在!'); 
+                req.flash('error', '用户不存在!');
                 return res.redirect('/login');//用户不存在则跳转到登录页
             }
             //检查密码是否一致
             if (user.password != password) {
-                req.flash('error', '密码错误!'); 
+                req.flash('error', '密码错误!');
                 return res.redirect('/login');//密码错误则跳转到登录页
             }
-            
+
             req.flash('name', user.name);
             //用户名密码都匹配后，将用户信息存入 session
             req.session.user = user;
@@ -110,7 +109,7 @@ module.exports = function(app) {
 
     app.get('/post', checkLogin);
     app.get('/post', function (req, res) {
-        res.render('post', { 
+        res.render('post', {
             title: '发表',
             user: req.session.user,
             success: req.flash('success').toString(),
@@ -124,7 +123,7 @@ module.exports = function(app) {
             post = new Post(currentUser.name, req.body.title, req.body.post);
         post.save(function (err) {
             if (err) {
-                req.flash('error', err); 
+                req.flash('error', err);
                 return res.redirect('/');
             }
             req.flash('success', '发布成功!');
@@ -142,10 +141,10 @@ module.exports = function(app) {
     app.use(function (req, res) {
         res.render("404");
     });
-    
+
     function checkLogin(req, res, next) {
         if (!req.session.user) {
-            req.flash('error', '未登录!'); 
+            req.flash('error', '未登录!');
             res.redirect('/login');
         }
         next();
@@ -153,7 +152,7 @@ module.exports = function(app) {
 
     function checkNotLogin(req, res, next) {
         if (req.session.user) {
-            req.flash('error', '已登录!'); 
+            req.flash('error', '已登录!');
             res.redirect('back');//返回之前的页面
         }
         next();
